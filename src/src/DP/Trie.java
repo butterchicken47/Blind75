1fb5a9e1b516dbb3ff11c0ed0b0ea5e85861ee3f
butterchicken47 <dibyanshdv937@gmail.com>
2025-05-05 11:53:40 +0530

    trie and word break

---
+++ src/src/DP/Trie.java
@@ -0,0 +1,60 @@
+package DP;
+
+public class Trie {
+    static class Node {
+        Node[] children ;
+        boolean eow; // end of word
+        public Node (){
+            children = new Node[26];
+            eow = false;
+        }
+    }
+//    static Node root = new Node();
+    public static void insert (String word , Node root){
+        Node curr = root;
+        for (int i = 0; i< word.length(); i ++){
+            int index = word.charAt(i) - 'a';
+            if(curr.children[index] == null){
+                curr.children[index] = new Node();
+            }
+            if(i == word.length() -1 )
+                curr.children[index].eow = true;
+            curr = curr.children[index];
+        }
+    }
+    public static boolean search (String word , Node root){
+        Node curr =root;
+        for (int i = 0; i< word.length() ; i ++){
+            int index = word.charAt(i) - 'a';
+
+            if(curr.children[index] == null){
+                return false;
+            }
+            if(i == word.length() -1 && curr.children[index].eow == false)
+                return false;
+
+            curr = curr.children[index];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"cats" , "sand" , "butt"};
+        Node root = new Node();
+        for (String word : words)
+            insert(word , root);
+
+        for (String word : words) {
+            System.out.println("Trying to search word : " + word + ": " + search(word, root));
+        }
+        String random = "random";
+
+        System.out.println("Trying to search word : " + random + ": " + search(random, root));
+
+
+
+
+
+    }
+
+}
--- src/src/DP/wordBreakProblem.java
+++ src/src/DP/wordBreakProblem.java
@@ -1,9 +1,10 @@
 package DP;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 
-class wordBreakProblem {
+class wordBreakProblem  extends Trie{
 
     public static void main (String[] args){
 
@@ -11,19 +12,70 @@ class wordBreakProblem {
 //        List<String> dictList = Arrays.asList("leet" , "Code");
 
         String s = "catsand";
+//        s = "cats";
         List<String> dictList = Arrays.asList("cat" , "cats" , "and");
 
         System.out.println("RESPONSE IS " + wordBreak(s , dictList));
 
     }
 
+        boolean[] memo;
+        public boolean wordBreakByLeetCode(String s, List<String> wordDict) {
+            memo = new boolean[s.length() + 1];
+            return wordBreak(s, wordDict, 0);
+        }
+        boolean wordBreak(String s, List<String> wordDict, int k) {
+            if (k == s.length())
+                return true;
+
+            if (memo[k])
+                return false;
+
+            for (String word : wordDict) {
+                if (s.startsWith(word, k)) {
+                    memo[k] = true;
+                    if(wordBreak(s, wordDict, k + word.length())) return true;
+                }
+            }
+            return false;
+        }
+
     public static boolean wordBreak(String s, List<String> wordDict) {
+        Node root = new Node();
         char [] arr = s.toCharArray();
         int l= 0;
         int r = 1;
         boolean wordFound = false;
 
-        return f (s , l , r , wordDict );
+        for (String word : wordDict)
+            insert(word , root);
+
+
+        return searchSubstring(s , root );
+//        return f (s , l , r , wordDict );
+
+    }
+    public static  HashMap<String , Boolean> map = new HashMap<>();
+
+    public static boolean searchSubstring (String key , Node trie ){
+        if(key.isEmpty())
+            return true;
+
+        for (int i = 1 ;i <= key.length(); i ++){
+            String firstPart = key.substring(0,i);
+            String secondPart = key.substring(i);
+            Boolean secondPartBool = false;
+            if(! map.containsKey(secondPart)) {
+                secondPartBool = searchSubstring(secondPart, trie);
+                map.put(secondPart , secondPartBool);
+            }else
+                secondPartBool = map.get(secondPart);
+
+            if(search(firstPart , trie) && secondPartBool) {
+                return true;
+            }
+        }
+        return false;
 
     }
 
